8a7edb21b6f40502ef9ed210c3a5d6f5b86e780a
Manimaran B <manimaranmca0710@gmail.com>
2026-07-09 13:18:46 +0530

    PlaywrightAssignment

---
+++ Week1Day1/variabledeclaration.js
@@ -0,0 +1,17 @@
+//var allows redeclaration and reassignment
+var city="Kumbakonam"
+var city="Chennai" //latest declared value will be print
+console.log(city)
+console.log(typeof city) 
+
+var pincode=612202
+console.log(pincode)
+
+//let doesnot allows redeclaration and reassignment
+let name="Manimaran"
+name="Walter white" //variable vaule will be change
+console.log(name)
+
+//const is fixed we can't reassign and redeclare
+const pi=3.14 //value is fixed
+console.log(pi)
