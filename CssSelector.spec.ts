a8cc32554b9e3dde0e38d6a00997875d03d0182e
manimaranb07 <manimaranmca0710@gmail.com>
2026-07-24 16:58:41 +0530

    Delete CssSelector.spec.ts

--- CssSelector.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test} from "@playwright/test";
-
-test ('Css Selector' ,async ({page}) => {
-     await page.goto("https://login.salesforce.com/?locale=in")
-     await page.locator('#username').fill('dilipkumar.rajendran@testleaf.com')
-     await page.locator('#password').fill('TestLeaf@2025')
-
-     await page.locator('input[id="Login"]').click
-})
