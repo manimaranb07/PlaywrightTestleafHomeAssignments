f6397c75b13d6653cf63d35051c31d8a8010e37e
manimaranb07 <manimaranmca0710@gmail.com>
2026-08-04 13:31:13 +0530

    Multiple file upload

--- HomeAssignment/multipleFileUpload.spec.ts
+++ HomeAssignment/multipleFileUpload.spec.ts
@@ -5,13 +5,16 @@ test('Multiple file upload',async ({page}) => {
     //Navigate to file page
     await page.goto('https://www.leafground.com/file.xhtml')
     
-    //Click on
+    //Click on advance upload
     const advanceUpload=await page.locator('(//input[@type="file"])[2]')
 
+    //Using Path module to construct file paths
     await advanceUpload.setInputFiles([path.join('Data/QA-Testing-1024x533.jpg'),path.join('Data/qa.jpg')])
 
+    //Uploading two files
     await page.locator('//span[text()="Upload"]').click()
 
+    //Validating file uploaded
     const verifyfileUpload = await page.locator('//span[text()="Successful"]').innerText()
     console.log("The file1 uploaded is:",verifyfileUpload);
 
