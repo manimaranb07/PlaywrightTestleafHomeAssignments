58b623526b3b00810b2f4f01b1d72b31bd3bab38
manimaranb07 <manimaranmca0710@gmail.com>
2026-08-13 10:04:36 +0530

    Delete new-folder directory

--- new-folder/AccountPage.ts
+++
@@ -1,7 +0,0 @@
-import { homePage } from "./HomePage";
-
-export class accountPage extends homePage{
-    async clickonCreateAccount(){
-        await this.page.locator('//a[text()="Create Account"]').click()
-    }
-}
--- new-folder/HomePage.ts
+++
@@ -1,8 +0,0 @@
-import { welcomPage } from "./WelcomePage";
-
-export class homePage extends welcomPage{
-
-    async clickonAccounts(){
-        await this.page.locator('//a[text()="Accounts"]').click()
-    }
-}
